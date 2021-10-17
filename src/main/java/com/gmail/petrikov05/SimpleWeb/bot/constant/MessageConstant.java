37d2473bdd42127a364b6e3b11f710f43673c582
ashnarkevich <petrikov05@gmail.com>
2021-10-18 00:19:42 +0300

    update

--- src/main/java/com/gmail/petrikov05/SimpleWeb/bot/constant/MessageConstant.java
+++ src/main/java/com/gmail/petrikov05/SimpleWeb/bot/constant/MessageConstant.java
@@ -2,7 +2,7 @@ package com.gmail.petrikov05.SimpleWeb.bot.constant;
 
 public interface MessageConstant {
 
-    String MESSAGE_FOR_COMMAND_START = "Shnarkevich.by к Вашим услугам. Бот-консультант рад с Вами пообщаться.";
+    String MESSAGE_FOR_COMMAND_START = "Бот-консультант рад с Вами пообщаться.";
     String MESSAGE_FOR_WRONG_COMMAND = "I don't know this command. I only know command: /start and /help";
     String MESSAGE_FOR_COMMAND_HELP = "Use command /start";
 
